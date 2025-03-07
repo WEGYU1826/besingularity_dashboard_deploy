9f2ca2d5c390b5a684d91b664fa6fdda0763b9a5
wegyu <awhm1826@gmail.com>
2025-03-07 14:49:55 +0300

    1.0.4: sign up via invitation is added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2018023096"
+    serviceWorkerVersion: "1912164852"
   }
 });
