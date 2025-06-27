05635dd202da95ebd1773f51edfa15fd93653a01
wegyu <awhm1826@gmail.com>
2025-06-27 09:17:17 +0300

    bsg admin dasboard mass invitee bug fix deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "828709116"
+    serviceWorkerVersion: "473562848"
   }
 });
